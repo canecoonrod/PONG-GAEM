293434836adca20a6225f783aecb941e546c34c1
canecoonrod <96085535+canecoonrod@users.noreply.github.com>
2022-01-02 23:45:55 -0600

    Update pong-audio.js

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -56,12 +56,12 @@ soundArray.push(scoreSound);
 export var ambientSound = new soundFile("Adventure_Meme.mp3");
 soundArray.push(ambientSound);
 ambientSound.player.loop = true; //turn on looping
-ambientSound.player.volume.value = 9; //turn down volume
+ambientSound.player.volume.value = 20; //turn down volume
 
 export var adventureMusic = new soundFile("Eight_Bit_Village_Loop.mp3");
 soundArray.push(adventureMusic);
 adventureMusic.player.loop = true;
-adventureMusic.player.volume.value = -16;
+adventureMusic.player.volume.value = -4;
 
 export var villageMusic = new soundFile("phaserDown2.mp3");
 soundArray.push(villageMusic);
